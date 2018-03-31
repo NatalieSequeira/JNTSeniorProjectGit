3b3337c4a1e02e26768f24f82cc5be423cee1016
Tom <thomaszorn@thomass-mbp.home>
2018-03-31 14:17:58 -0400

    Added task object, to merge properly

---
+++ JNTSeniorProjectXCode/TaskObject.swift
@@ -0,0 +1,9 @@
+//
+//  TaskObject.swift
+//  JNTSeniorProjectXCode
+//
+//  Created by Thomas Zorn on 3/31/18.
+//  Copyright © 2018 Tom, Jack, and Natalie. All rights reserved.
+//
+
+import Foundation
